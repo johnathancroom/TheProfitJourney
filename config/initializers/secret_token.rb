ece0f4463fbbd97e2d1aca5206edd1547c83ab26
Johnathan Croom <johnathancroom@gmail.com>
2012-08-18 13:35:36 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheProfitJourney::Application.config.secret_token = '106a37f824a57b2aec5085f7a184eda8d8401bc470b82bbaae2d3fd18c4e13e201a130f74d93ad8c290ec0c31f5ee72d5603d2c453e8a7992c6c94bff8d3eeaf'
