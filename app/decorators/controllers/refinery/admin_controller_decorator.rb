0de203a3ed4f03823827db39dc470624b6d76bd0
Johnathan Croom <johnathancroom@gmail.com>
2012-08-21 11:59:38 -0700

    Site bar and backend only available to superuser

---
+++ app/decorators/controllers/refinery/admin_controller_decorator.rb
@@ -0,0 +1,10 @@
+Refinery::AdminController.class_eval do
+  before_filter :check_admin_permissions
+
+  # Redirect back if you don't have admin permissions
+  def check_admin_permissions
+    if !current_refinery_user.has_role?(:superuser)
+      redirect_to root_path
+    end
+  end
+end
