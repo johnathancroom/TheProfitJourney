986c911a44db78e6e59afa6de72c9651aacba43c
Johnathan Croom <johnathancroom@gmail.com>
2012-09-14 13:13:30 -0700

    Workshop can be changed/added in user edit page

--- app/decorators/models/refinery/user_decorator.rb
+++ app/decorators/models/refinery/user_decorator.rb
@@ -1,4 +1,6 @@
 Refinery::User.class_eval do
   has_one :customer, :class_name => 'Refinery::Akouo::Customer'
   belongs_to :workshop, :class_name => "Refinery::Workshops::Workshop"
+
+  attr_accessible :workshop_id
 end
