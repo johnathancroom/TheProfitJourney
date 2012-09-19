b13f0fad5c6509e669c37b7af2e00dc3c963b3ca
Johnathan Croom <johnathancroom@gmail.com>
2012-09-19 11:29:43 -0700

    DRYing out the find_page method in SheetController

--- vendor/extensions/akouo/app/controllers/refinery/akouo/sheet_controller.rb
+++ vendor/extensions/akouo/app/controllers/refinery/akouo/sheet_controller.rb
@@ -3,9 +3,9 @@ module Refinery
     class SheetController < ::ApplicationController
 
       before_filter :find_customer
-      before_filter :find_page_start, :only => [:"start/show", :"start/edit"]
-      before_filter :find_page_vision, :only => [:"vision/show", :"vision/edit"]
-      before_filter :find_page_last_year, :only => [:"last_year/show", :"last_year/edit"]
+      before_filter :only => [:"start/show", :"start/edit"] { |c| c.find_page("/akouo/start") }
+      before_filter :only => [:"vision/show", :"vision/edit"] { |c| c.find_page("/akouo/vision") }
+      before_filter :only => [:"last_year/show", :"last_year/edit"] { |c| c.find_page("/akouo/last_year") }
 
       def start_update
         if @customer.update_attributes(params[:customer])
@@ -25,14 +25,8 @@ module Refinery
         @customer = current_refinery_user.customer
       end
 
-      def find_page_start
-        @page = ::Refinery::Page.where(:link_url => "/akouo/start").first
-      end
-      def find_page_vision
-        @page = ::Refinery::Page.where(:link_url => "/akouo/vision").first
-      end
-      def find_page_last_year
-        @page = ::Refinery::Page.where(:link_url => "/akouo/last_year").first
+      def find_page(link_url)
+        @page = ::Refinery::Page.where(:link_url => link_url).first
       end
 
     end
