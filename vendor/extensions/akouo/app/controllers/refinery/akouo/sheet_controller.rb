e67a61a4cac350cc1a895da48252f0d1bec94698
Johnathan Croom <johnathancroom@gmail.com>
2012-09-20 12:33:21 -0700

    Scaffolding Last Year

--- vendor/extensions/akouo/app/controllers/refinery/akouo/sheet_controller.rb
+++ vendor/extensions/akouo/app/controllers/refinery/akouo/sheet_controller.rb
@@ -2,27 +2,34 @@ module Refinery
   module Akouo
     class SheetController < ::ApplicationController
 
-      before_filter :find_customer
+      SAVED_MESSAGE = "Successfully saved!"
+
+      before_filter :find_models
       before_filter :only => [:"start/show", :"start/edit"] { |c| c.find_page("/akouo/start") }
       before_filter :only => [:"vision/show", :"vision/edit"] { |c| c.find_page("/akouo/vision") }
       before_filter :only => [:"last_year/show", :"last_year/edit"] { |c| c.find_page("/akouo/last_year") }
 
       def start_update
         if @customer.update_attributes(params[:customer])
-          redirect_to refinery.akouo_start_path, :notice => "Successfully saved!"
+          redirect_to refinery.akouo_start_path, :notice => SAVED_MESSAGE
         else
-          render :edit
+          render :"start/edit"
         end
       end
 
       def last_year_update
-
+        if @last_year.update_attributes(params[:customer])
+          redirect_to refinery.akouo_last_year_path, :notice => SAVED_MESSAGE
+        else
+          render :"last_year/edit"
+        end
       end
 
     protected
 
-      def find_customer
+      def find_models
         @customer = current_refinery_user.customer
+        @last_year = current_refinery_user.last_year
       end
 
       def find_page(link_url)
