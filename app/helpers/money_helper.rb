1ca41e0b02b2d6c0f58d3c3c618dbd17e616c0c7
Johnathan Croom <johnathancroom@gmail.com>
2012-09-26 09:27:04 -0700

    money_field helper method shows label if disabled

--- app/helpers/money_helper.rb
+++ app/helpers/money_helper.rb
@@ -5,8 +5,15 @@ module ActionView
   module Helpers
     class FormBuilder
       def money_field(method, options = {})
+        # Force cents
         options = options.merge({:step => 0.01})
-        @template.number_field(@object_name, method, objectify_options(options))
+
+        # Show label if field is disabled
+        if options[:disabled] == true
+          @template.label(@object_name, method, options[:value], objectify_options(options))
+        else
+          @template.number_field(@object_name, method, objectify_options(options))
+        end
       end
     end
   end
